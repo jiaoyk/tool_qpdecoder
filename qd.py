1fe25432e90e37d2a4aadb4e68d22b5c0f30cf5d
Sam Michaels <swmichaels@gmail.com>
2016-02-02 00:37:44 -0600

    made it work on stdin and just output instead

--- qd.py
+++ qd.py
@@ -3,21 +3,15 @@
 import quopri
 import argparse
 import codecs
-parser = argparse.ArgumentParser(description='give me some files')
+import fileinput
 
-parser.add_argument("input", help="Quoted Printable input file")
-parser.add_argument("output", help="Decoded output file")
+s = ''
+for line in fileinput.input():
+    s += line
 
-args = parser.parse_args()
-
-in_file = open(args.input, mode='r')
-quoted_text = in_file.read()
-in_file.close()
+quoted_text = s
 
 decoded = quopri.decodestring(quoted_text)
 decoded = decoded.replace('\r\n', '\n');
-decoded = decoded.decode('utf-8', 'replace')
 
-out_file = codecs.open(args.output, encoding='utf-8', mode='w+')
-out_file.write(decoded)
-out_file.close()
+print decoded
